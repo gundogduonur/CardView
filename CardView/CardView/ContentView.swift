2c15e5f17ee489f298d767843a538b4afde8b395
gundogduonur <gundogduonur@outlook.com.tr>
2022-10-15 13:28:36 +0300

    added image

--- CardView/CardView/ContentView.swift
+++ CardView/CardView/ContentView.swift
@@ -13,7 +13,9 @@ struct ContentView: View {
     var body: some View {
         VStack
         {
-            
+            Image("watch")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
         }
         }
     }
